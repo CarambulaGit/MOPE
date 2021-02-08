8a81b29f9379239e5aca131e42d4550a8e5945dc
Carambula <antibog6@gmail.com>
2021-02-08 19:01:42 +0200

    add Utils.cs

---
+++ MOPEUtils/Utils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MOPEUtils {
+    public static class RandomExtension {
+        public static float Range(this Random random, float randomMin, float randomMax) {
+            return (float) random.NextDouble() * (randomMax - randomMin) + randomMin;
+        }
+    }
+
+    public static class ArrayExtension {
+        public static void Print<T>(this T[][] arr) {
+            foreach (var row in arr) {
+                foreach (var element in row) {
+                    Console.Write($"{element} \t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        public static void PrintRevert<T>(this T[][] arr) {
+            for (var j = 0; j < arr[0].Length; j++) {
+                foreach (var column in arr) {
+                    Console.Write($"{column[j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+    
+    public static class ListExtension {
+    
+    }
+}
